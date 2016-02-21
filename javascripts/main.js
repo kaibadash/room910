26222ddc677247f0e72435fe0040168ab9928a72
kaiba <kaiba@desktop.local>
2016-02-22 02:13:10 +0900

    WIP design

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,9 @@
-Vue.filter('datetime', function (unixtime) {
-  console.log(unixtime);
-  return new Date( unixtime * 1000 )
+Vue.filter("datetime", function (unixtime) {
+  return new Date(unixtime).toLocaleString()
+});
+Vue.filter("rerativeTime", function (unixtime) {
+  var diff = new Date().getTime() - unixtime;
+  return Math.floor(diff / 1000);
 });
 
 var vue = new Vue({
@@ -13,7 +16,8 @@ var vue = new Vue({
     var humanSensorDataList = new Array();
     var milkcocoa = new MilkCocoa("blueiks5jeta.mlkcca.com");
     milkcocoa.dataStore("human_sensor").stream().sort("desc").size(10).next(function(err, dataList) {
-      console.log(dataList);
+      // Data format:
+      // {"id": "XXX","timestamp":1456045078635,"value":"{\"v\":\"1\"}"}
       self.humanSensorDataList = self.humanSensorDataList.concat(dataList);
     });
   }
