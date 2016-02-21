fa78de35cadda5e9eaf0c67911d1ce7599d1878c
kaiba <kaiba@desktop.local>
2016-02-21 18:41:23 +0900

    init

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,21 @@
-console.log('This would be the main JS file.');
+Vue.filter('datetime', function (unixtime) {
+  console.log(unixtime);
+  return new Date( unixtime * 1000 )
+});
+
+var vue = new Vue({
+  el: "#human_sensor_data_list",
+  data: {
+    humanSensorDataList: []
+  },
+  created: function() {
+    var self = this;
+    var humanSensorDataList = new Array();
+    var milkcocoa = new MilkCocoa("blueiks5jeta.mlkcca.com");
+    milkcocoa.dataStore("human_sensor").stream().sort("desc").size(10).next(function(err, dataList) {
+      console.log(dataList);
+      self.humanSensorDataList = self.humanSensorDataList.concat(dataList);
+    });
+  }
+});
+
